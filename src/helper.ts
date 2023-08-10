3e2ee024fa50696088bb9d25696bfc50c580c649
Junli Li <jungle216@126.com>
2023-08-10 22:19:07 +0800

    decode credential

--- src/helper.ts
+++ src/helper.ts
@@ -1,6 +1,108 @@
 import crypto from "crypto";
+import cbor from "cbor";
 
 function generateRandomUserId() {
   return crypto.randomBytes(8).toString("hex");
 }
-export { generateRandomUserId };
+
+function verifyAuthenticatorDataAndAttestation(authenticatorData: string, attestationObject: string, clientDataJSON: string) {
+  const authenticatorDataBuffer = base64URLDecode(authenticatorData);
+  const attestationObjectBuffer = base64URLDecode(attestationObject);
+  const clientDataJSONBuffer = base64URLDecode(clientDataJSON);
+
+  // Verify authenticator data
+  // Authenticator data includes flags, counters, and other information
+  // Ensure flags are set correctly and counters are valid
+  const { flags, counter } = extractAuthenticatorData(authenticatorDataBuffer);
+  console.log("flags", flags, "counter", counter);
+
+  // Verify attestation object
+  // This involves parsing and validating the attestation object
+  // Ensure attestation statement format, public key, and other details
+
+  // Verify client data JSON
+  // This involves parsing and validating the client data JSON
+  // Ensure challenge matches, origin is correct, and type is webauthn.get
+
+  if (verifyClientDataJSON(clientDataJSONBuffer) && verifyAttestationObject(attestationObjectBuffer)) {
+    return true;
+  }
+
+  return false;
+}
+
+function base64URLDecode(input: string) {
+  // Convert base64 URL-safe to base64 (add padding if needed)
+  const base64 = input.replace(/-/g, "+").replace(/_/g, "/");
+
+  // Convert base64 to binary buffer
+  const binaryBuffer = Buffer.from(base64, "base64");
+
+  // Convert binary buffer to Uint8Array
+  return new Uint8Array(binaryBuffer);
+}
+
+function extractAuthenticatorData(authenticatorData: Uint8Array) {
+  // Authenticator data is a binary buffer (Uint8Array)
+  // It has a specific structure defined by the FIDO2/WebAuthn specification
+
+  // Flags (bit flags indicating various properties)
+  const flags = authenticatorData[32]; // Flags are at index 32
+
+  // Counter (a counter value indicating the usage of the authenticator)
+  const counter = authenticatorData.slice(33, 37); // Counter is at index 33 to 36
+
+  // Other data may be included in the authenticator data
+  // For example, attested credential data, extensions, etc.
+
+  return {
+    flags,
+    counter,
+  };
+}
+
+function verifyClientDataJSON(clientDataJSON: Uint8Array, expectedChallenge: string = "dT3857_x", expectedOrigin: string = "http://localhost:3000") {
+  const clientDataString = new TextDecoder().decode(clientDataJSON);
+  const clientData = JSON.parse(clientDataString);
+
+  console.log("clientData", clientData);
+
+  // Verify challenge
+  const challenge = clientData.challenge;
+  if (challenge !== expectedChallenge) {
+    return false;
+  }
+
+  // Verify origin
+  const origin = clientData.origin;
+  if (origin !== expectedOrigin) {
+    return false;
+  }
+
+  // Verify type (should be "webauthn.get")
+  const type = clientData.type;
+  if (type !== "webauthn.create") {
+    return false;
+  }
+
+  return true;
+}
+
+function verifyAttestationObject(attestationObject: Uint8Array) {
+  // Parse attestation object (assuming it's CBOR encoded)
+  const attestationObjectArray = new Uint8Array(attestationObject);
+  const parsedAttestationObject = cbor.decode(attestationObjectArray);
+  console.log("parsedAttestationObject", parsedAttestationObject);
+
+  // Verify that the attestation object includes "fmt" field (format)
+  if (!parsedAttestationObject.hasOwnProperty("fmt")) {
+    return false;
+  }
+
+  // Perform attestation format-specific verification
+  // This could involve verifying the attestation statement, certificate, etc.
+
+  return true;
+}
+
+export { generateRandomUserId, verifyAuthenticatorDataAndAttestation, extractAuthenticatorData };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
-
 import cors from "cors";
-import { generateRandomUserId } from "./helper";
+import { verifyAuthenticatorDataAndAttestation } from "./helper";
 
 const app = express();
 app.use(express.json());
@@ -9,7 +8,14 @@ app.use(express.json());
 // Enable CORS for all routes
 app.use(cors());
 
-const registeredUsers: Record<string, { attestation: string }> = {};
+const registeredUsers: Record<string, { attestation: string; clientData: string }> = {
+  "12": {
+    attestation:
+      "o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YViYSZYN5YgOjGh0NBcPZHZgW4/krrmihjLHmVzzuoMdl2NdAAAAAAAAAAAAAAAAAAAAAAAAAAAAFJ3jImqEXLFLrkNRfrOncBR9P3OopQECAyYgASFYIEJP95sicsTsvFh0Fxfql2DjoTD5z1GKDGCdTIEJS+piIlggqLIX3pbhZlOhcYhVI4EXb1E2tl0guLS/UTuRaytKCdo=",
+    clientData:
+      "eyJ0eXBlIjoid2ViYXV0aG4uY3JlYXRlIiwiY2hhbGxlbmdlIjoiZFQzODU3X3giLCJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJjcm9zc09yaWdpbiI6ZmFsc2V9",
+  },
+};
 
 app.post("/register", (req, res) => {
   const { attestationObject, clientDataJSON, username } = req.body;
@@ -29,7 +35,7 @@ app.post("/login", (req, res) => {
   }
 
   // Simulate a login process by comparing authenticator data
-  if (registeredUsers[username].attestation === authenticatorData) {
+  if (verifyAuthenticatorDataAndAttestation(authenticatorData, registeredUsers[username].attestation, registeredUsers[username].clientData)) {
     return res.send(`User logged in successfully for ${username}`);
   } else {
     return res.status(401).send("Authentication failed.");
